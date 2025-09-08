5a9f523fafa93ca0c252ad36ca052fdafbe299d0
najwaaz-zahra34rpl-tech <najwa_az-zahra_34rpl@student.smktelkom-mlg.sch.id>
2025-09-08 14:59:15 +0700

    Add files via upload

--- BookTester.java
+++ BookTester.java
@@ -3,10 +3,7 @@ public class BookTester {
         Book myBook = new Book("The Lord of the Rings", 1954, 25.50);
         myBook.print();
         
-        System.out.println("\n--- Updating book details ---");
-        myBook.setPrice(30.00);
-        myBook.setYear(1965);
-        myBook.print();
+        ;
     }
     
 }
